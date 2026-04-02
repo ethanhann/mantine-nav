953027adc307ccb39e89a873d22ec63ee4889699
Ethan Hann <ethanhann@gmail.com>
2026-04-02 12:15:47 -0400

    Disable Storybook telemetry in configuration file.

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,11 +1,12 @@
-import type { StorybookConfig } from '@storybook/react-vite';
+import type { StorybookConfig } from "@storybook/react-vite";
 
 const config: StorybookConfig = {
-  stories: ['../stories/**/*.stories.tsx'],
-  addons: [
-    '@storybook/addon-a11y',
-  ],
-  framework: '@storybook/react-vite',
+	stories: ["../stories/**/*.stories.tsx"],
+	core: {
+		disableTelemetry: true,
+	},
+	addons: ["@storybook/addon-a11y"],
+	framework: "@storybook/react-vite",
 };
 
 export default config;
