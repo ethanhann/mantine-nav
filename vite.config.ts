e2f6c4cbc0db0a3081e073e2dd6c08e06cda6f25
Ethan Hann <ethanhann@gmail.com>
2026-06-02 08:41:50 -0400

    bump version to 0.3.8 and replace vite-plugin-dts with unplugin-dts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { resolve } from "node:path";
 import react from "@vitejs/plugin-react";
+import dts from "unplugin-dts/vite";
 import { defineConfig } from "vite";
-import dts from "vite-plugin-dts";
 
 export default defineConfig({
 	plugins: [
