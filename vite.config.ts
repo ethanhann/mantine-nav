ce3368f2facf535e82d2e790177561a180cf1c2c
Ethan Hann <ethanhann@gmail.com>
2026-06-23 12:34:51 -0400

    Update Vite config to exclude `unplugin-dts` when running Storybook

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,10 @@ import react from "@vitejs/plugin-react";
 import dts from "unplugin-dts/vite";
 import { defineConfig } from "vite";
 
-export default defineConfig(({ command }) => ({
+export default defineConfig({
 	plugins: [
 		react(),
-		command === "build" &&
+		!process.env.STORYBOOK &&
 			dts({
 				include: ["src"],
 				exclude: ["**/*.test.*", "**/__integration__/**", "**/test-setup.*"],
