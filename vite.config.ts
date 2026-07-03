6c1433303710ef0b773783394c7cc074b00a6769
Ethan Hann <ethanhann@gmail.com>
2026-07-03 09:53:54 -0400

    Update Vite config: add `rollup-preserve-directives` plugin and remove CommonJS build format

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
 import { resolve } from "node:path";
 import react from "@vitejs/plugin-react";
+import preserveDirectives from "rollup-preserve-directives";
 import dts from "unplugin-dts/vite";
 import { defineConfig } from "vite";
 
 export default defineConfig({
 	plugins: [
 		react(),
+		preserveDirectives(),
 		!process.env.STORYBOOK &&
 			dts({
 				include: ["src"],
@@ -17,16 +19,11 @@ export default defineConfig({
 				insertTypesEntry: true,
 			}),
 	],
-	css: {
-		modules: {
-			localsConvention: "camelCase",
-		},
-	},
 	build: {
 		lib: {
 			entry: resolve(__dirname, "src/index.ts"),
 			name: "Nav",
-			formats: ["es", "cjs"],
+			formats: ["es"],
 			fileName: "index",
 		},
 		rollupOptions: {
